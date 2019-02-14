d9fb8c5f8559d62c963a68db26195c1d211bd975
alezubokoff <alezubokoff@gmail.com>
2019-02-13 23:03:54 -0200

    Categoria

--- src/main/java/com/zubokoff/cursomc/resouces/CategoriaResource.java
+++ src/main/java/com/zubokoff/cursomc/resouces/CategoriaResource.java
@@ -1,16 +1,27 @@
 package com.zubokoff.cursomc.resouces;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.zubokoff.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest esta funcionando!";
+	public List<Categoria> listar() {
+		
+		List<Categoria> categorias = new ArrayList<>();
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		categorias.add(cat1);
+		categorias.add(cat2);
+		return categorias;
 	}
 
 }
