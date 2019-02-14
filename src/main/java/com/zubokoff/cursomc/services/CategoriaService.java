d7167bb1c18b302cbd806c4070e002225b8c6b2d
alezubokoff <alezubokoff@gmail.com>
2019-02-14 00:06:09 -0200

    Criando Repository e Service para categoria

---
+++ src/main/java/com/zubokoff/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.zubokoff.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.zubokoff.cursomc.domain.Categoria;
+import com.zubokoff.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+
+}
