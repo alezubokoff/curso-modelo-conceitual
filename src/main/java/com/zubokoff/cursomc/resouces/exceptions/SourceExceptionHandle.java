7c3067e4d2ffc4e4247538377946644289f250ab
alezubokoff <alezubokoff@gmail.com>
2019-02-19 22:04:28 -0300

    Ajustes no endpoint /categorias/{id}

---
+++ src/main/java/com/zubokoff/cursomc/resouces/exceptions/SourceExceptionHandle.java
@@ -0,0 +1,21 @@
+package com.zubokoff.cursomc.resouces.exceptions;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.zubokoff.cursomc.services.exceptios.ObjectNotFoundException;
+
+@ControllerAdvice
+public class SourceExceptionHandle {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest req) {
+		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+
+}
--- src/main/java/com/zubokoff/cursomc/services/CategoriaService.java
+++ src/main/java/com/zubokoff/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.zubokoff.cursomc.domain.Categoria;
 import com.zubokoff.cursomc.repositories.CategoriaRepository;
+import com.zubokoff.cursomc.services.exceptios.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,10 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+//		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objecto não encontrado! id: " +
+			id + ", Tipo: " + Categoria.class.getName()));
+		
 	}
 
 }
