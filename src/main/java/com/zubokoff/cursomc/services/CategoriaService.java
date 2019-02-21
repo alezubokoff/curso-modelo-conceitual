2c1a83ec99b1d7fa3182b3d847657686873f2436
alezubokoff <alezubokoff@gmail.com>
2019-02-20 22:48:40 -0300

    categoria service

--- src/main/java/com/zubokoff/cursomc/services/CategoriaService.java
+++ src/main/java/com/zubokoff/cursomc/services/CategoriaService.java
@@ -17,7 +17,6 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-//		return obj.orElse(null);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objecto não encontrado! id: " +
 			id + ", Tipo: " + Categoria.class.getName()));
 		
